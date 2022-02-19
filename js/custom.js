bc3134dbcf0cbfde546b0435e8f17320c5a17ee2
prokash Pul <pprokash102@gmail.com>
2022-02-19 16:52:32 +0600

    update balance , save section

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,34 @@
 // input field function
 const inputData = (inputFieldName) => {
-    const inputField = document.getElementById(inputFieldName + "-input");
-    let inputValue = parseInt(inputField.value);
-    inputField.value = 0;
-    return inputValue;
+        const inputField = getId(inputFieldName + "-input");
+        let inputValue = parseInt(inputField.value);
+        inputField.value = 0;
+        if (inputValue >= 0) {
+            return inputValue;
+        } else {
+            alert(`you are wrong data input in ${inputFieldName} .`);
+        }
 
-}
+    }
+    // function getid
+const getId = (idName) => {
+    let innerText = document.getElementById(idName);
+    return innerText
+};
 
 // javascript events
-document.getElementById('calculate-button').addEventListener('click', () => {
-    const expensesBalance = document.getElementById('expenses-balance');
+getId('calculate-button').addEventListener('click', () => {
+    const expensesBalance = getId('expenses-balance');
     expensesBalance.innerText = inputData('rent') + inputData('food') + inputData('clothes');
     const totalExpenses = parseInt(expensesBalance.innerText);
     // return balance 
-    const balance = document.getElementById('balance');
+    const balance = getId('balance');
     balance.innerText = inputData('income') - totalExpenses;
+});
+
+getId('save-button').addEventListener('click', () => {
+    const balance = getId('balance')
+    const saveBalance = getId('save-balance').innerText = Math.round(parseInt(balance.innerText) * inputData('save') / 100);
+    getId('remaing-balance').innerText = Math.round(parseInt(balance.innerText) - saveBalance);
+
 });
